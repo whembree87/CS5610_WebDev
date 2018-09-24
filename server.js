76157f68ffe5bb833b8c714e705a6f12a0b42a1f
whembree87 <whembree87@gmail.com>
2018-09-23 17:16:10 -0700

    Reconfigured database

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
 app.use(session({
     secret: "this is the secret",
-    // secret : process.env.PASSPORT_SECRET,
     resave : true,
     saveUninitialized : true
 }));
@@ -22,25 +21,18 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// var connectionString = 'mongodb://127.0.0.1:27017/Project/';
-//
-// if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-//     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-//             process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-//             process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
-//             process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
-//             process.env.OPENSHIFT_APP_NAME;
-// }
+var connectionString = 'mongodb://whembree87:msdfall16@ds119078.mlab.com:19078/gesamt';
 
-var connectionString = 'mongodb://whembree87:msdfall16@ds211143.mlab.com:11143/heroku_38sgq0rq';
-
-var db = mongoose.connect(connectionString);
+var db = mongoose.connect(connectionString, function(err, res) {
+    if(err) {
+        console.log('Error connecting to the database. ' + err);
+    } else {
+        console.log('Connected to Database: ' + connectionString);
+    }
+});
 
 app.use(express.static(__dirname + '/public/Landing_Page'));
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-//require("./public/assignment5/server/app.js")(app, db, mongoose);
 require("./public/Project/server/app.js")(app, db, mongoose, bcrypt);
 
-app.listen(port, ipaddress);
+app.listen(process.env.PORT || 3000);
